a906fd21d06e326970fe8d9269a9ecafbccf7b59
Cezary Sosialuk <c.sosialuk1@gmail.com>
2025-02-26 15:31:31 +0100

    done

--- Fibonacci Word/Program.cs
+++ Fibonacci Word/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(FibonacciWord(10));
         }
+
+        static string FibonacciWord(int n)
+        { 
+            if (n < 2)
+            {
+                return "invalid";
+            }
+
+            string[] outarr = new string[n];
+
+            outarr[0] = "a";
+            outarr[1] = "b";
+
+            for (int i=2; i<n; i++)
+            {
+                outarr[i] = outarr[i - 1] + outarr[i - 2];
+            }
+
+            string output = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                output+=outarr[i] + ", ";
+            }
+
+            return output;
+        }
+
     }
 }
